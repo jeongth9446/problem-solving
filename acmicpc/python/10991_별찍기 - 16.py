23a02e8187f161cec08763e27787c7449e9a5e3f
jeongth9446 <52663419+jeongth9446@users.noreply.github.com>
2022-07-18 12:33:54 +0900

    [solved.ac]

---
+++ acmicpc/python/10991_별찍기 - 16.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+for i in range(n):
+    for j in range(n-i-1):
+        print(" ", end="")
+    if i == 0:
+        print("*")
+    else:
+        print("*", end="")
+        for j in range(i*2-1):
+            if j % 2 == 1:
+                print("*", end="")
+            else:
+                print(" ", end="")
+
+        print("*")
+
---
+++ acmicpc/python/10995_별 찍기 - 20.py
@@ -0,0 +1,8 @@
+n = int(input())
+
+for i in range(n):
+    if i % 2 == 1:
+        print(" ", end="")
+    for j in range(n):
+        print("* ", end="")
+    print()

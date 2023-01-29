f73c0a1e413fd939f1cf631b02cb482d35ce9ce8
jeongth9446 <52663419+jeongth9446@users.noreply.github.com>
2023-01-29 20:11:41 +0900

    [solved.ac] 쉬운문제

--- acmicpc/python/2729_이진수 덧셈.py
+++ acmicpc/python/2729_이진수 덧셈.py
@@ -1,13 +1,35 @@
 n = int(input())
 
-def dec2bin(x: int):
+
+def bin2dec(x: int):
     k = list(str(x))
+    k.reverse()
+    p = 1
+    res = 0
+    for item in k:
+        if item == '1':
+            res += p
+        p *= 2
+    return res
 
 
+def dec2bin(x: int):
+    s = list()
+    p = x
+
+    while p != 0:
+        s.append(str(p % 2))
+        p = p // 2
+    s.reverse()
+    if len(s) == 0:
+        return "0"
+    return "".join(s)
 
 
 for _ in range(n):
     a, b = list(map(int, input().split()))
 
-    c = dec2bin(a)
-    d = dec2bin(b)
+    c = bin2dec(a)
+    d = bin2dec(b)
+    print(dec2bin(c + d))
+    # print(c, d)

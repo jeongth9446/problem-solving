8cf3399cb893e535a3cd0c39e976c8752bd630f6
Taehun Jeong <taehuniy@gmail.com>
2023-07-30 22:35:24 +0900

    [쉬운문제]

---
+++ acmicpc/python/27889_특별한 학교 이름.py
@@ -0,0 +1,10 @@
+a = input()
+
+if a == "NLCS":
+    print("North London Collegiate School")
+elif a == "BHA":
+    print("Branksome Hall Asia")
+elif a == "KIS":
+    print("Korea International School")
+elif a == "SJA":
+    print("St. Johnsbury Academy")

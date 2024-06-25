3e8640628d323866c8818f866030ce087938e294
Jeong Taehun <taehuniy@gmail.com>
2024-06-25 20:38:01 +0900

    [26768, H4xOr]

---
+++ acmicpc/python/26768_H4x0r.py
@@ -0,0 +1,15 @@
+a = input()
+
+for i in a:
+    if i == 'a':
+        print("4", end="")
+    elif i == 'e':
+        print("3", end="")
+    elif i == 'i':
+        print("1", end="")
+    elif i == 'o':
+        print("0", end="")
+    elif i == 's':
+        print("5", end="")
+    else:
+        print(i, end="")
